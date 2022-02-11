df41fe5b80507fb437a28e09a3abb59a9403ccc1
NityaR3 <nityasrir3@gmail.com>
2022-02-11 22:33:30 +0530

    updated git

--- 3_implementation/main.c
+++ 3_implementation/main.c
@@ -1,42 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-int RandomNumbergenerator(int n)
-{
-    srand(time(NULL));
-    return rand() % n;
-}
-int greaternumber(char c1, char c2)
-{
-    if (c1 == c2)
-    {
-        return -1;
-    }
-    else if (c1 == 'r' && c2 == 's')
-    {
-        return 1;
-    }
-    else if (c2 == 'r' && c1 == 's')
-    {
-        return 0;
-    }
-    else if (c1 == 'p' && c2 == 'r')
-    {
-        return 1;
-    }
-    else if (c2 == 'p' && c1 == 'r')
-    {
-        return 0;
-    }
-    else if (c1 == 's' && c2 == 'p')
-    {
-        return 1;
-    }
-    else if (c2 == 's' && c1 == 'p')
-    {
-        return 0;
-    }
-}
 int main()
 {
     int playerScore = 0, compScore = 0, temp;
@@ -46,7 +7,7 @@ int main()
     printf("\t----------------------------------\n\n");
     for (int i = 0; i < 3; i++)
     {
-        // Take player input
+    
         printf("Press 1 for Rock, Press 2 for Paper, Press 3 for Scissors\n\n");
         printf("\tPlayer's turn: ");
         scanf("%d", &temp);
@@ -55,7 +16,7 @@ int main()
         printf(" -----------------\n");
         printf("| You choose: %c   |\n", playerChar);
         printf(" -----------------\n\n");
-        //computer generate
+     
         printf("Press 1 for Rock, Press 2 for Paper, Press 3 for Scissors\n\n");
         printf("\tComputer's turn\n");
         temp = RandomNumbergenerator(3) + 1;
@@ -63,7 +24,7 @@ int main()
         printf(" --------------------\n");
         printf("| Computer choose: %c |\n", compChar);
         printf(" --------------------\n\n");
-        // compater character and increment the score
+     
         if (greaternumber(compChar, playerChar) == 1)
         {
             compScore++;
@@ -80,10 +41,10 @@ int main()
             playerScore++;
             printf("\t\tYou Got It!\n\n");
         }
-        printf(" -------------\n");
+      
         printf("| You: %d      |\n", playerScore);
         printf("| Computer: %d |\n", compScore);
-        printf(" -------------\n\n");
+   
         printf("===========================================================\n\n");
     }
     printf(" -----------------\n");
@@ -92,25 +53,24 @@ int main()
     printf("|  You | Computer |\n");
     printf("|------|----------|\n");
     printf("|   %d  |    %d     |\n", playerScore, compScore);
-    printf(" -----------------\n\n");
-    // compare score
+   
     if (playerScore > compScore)
     {
-        printf("\n\t -------------------\n");
+       
         printf("\t| You Win the match |\n");
-        printf("\t -------------------\n");
+        
     }
     else if (playerScore < compScore)
     {
-        printf("\n\t ------------------------\n");
+      
         printf("\t| Computer Win the match |\n");
-        printf("\t ------------------------\n");
+      
     }
     else
     {
-        printf("\n\t -------------\n");
+       
         printf("\t| It's a draw |\n");
-        printf("\t -------------\n");
+       
     }
     return 0;
 }
---
+++ 3_implementation/rockpaperscissor.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int RandomNumbergenerator(int n)
+int RandomNumbergenerator(int n)
